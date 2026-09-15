4aefbf7ab5798e5bb5fd7161a8b67da7d6615972
bofa <68787392+bofaa-boop@users.noreply.github.com>
2026-09-15 20:24:04 +0200

    Update tornlogs_start.js

--- src/commands/torn/modules/tornlogs_start.js
+++ src/commands/torn/modules/tornlogs_start.js
@@ -2,7 +2,7 @@
 import { MessageFlags, PermissionFlagsBits } from 'discord.js';
 import { successEmbed } from '../../../utils/embeds.js';
 import { replyUserError, ErrorTypes } from '../../../utils/errorHandler.js';
-import { getConfig, setEnabled, startTracking } from '../../../utils/tornLogTracker.js';
+import { getGuildSettings, setGuildEnabled, startTracking } from '../../../utils/tornLogTracker.js';
 
 export default {
     async execute(interaction, config, client) {
@@ -13,16 +13,16 @@ export default {
             });
         }
 
-        const guildConfig = await getConfig(interaction.guildId);
-        if (!guildConfig) {
+        const settings = await getGuildSettings(interaction.guildId);
+        if (!settings) {
             return await replyUserError(interaction, {
                 type: ErrorTypes.CONFIGURATION,
-                message: 'Bitte zuerst `/tornlogs setup` ausführen.',
+                message: 'Bitte zuerst `/tornlogs setchannel` ausführen.',
             });
         }
 
         const started = startTracking(client, interaction.guildId);
-        await setEnabled(interaction.guildId, true);
+        await setGuildEnabled(interaction.guildId, true);
 
         return await interaction.reply({
             embeds: [successEmbed(started ? 'Torn-Log-Tracking gestartet.' : 'Läuft bereits.')],
