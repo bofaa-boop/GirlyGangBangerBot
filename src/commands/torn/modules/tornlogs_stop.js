26d4f7bde03acf05392112a3bc49c87511771320
bofa <68787392+bofaa-boop@users.noreply.github.com>
2026-09-15 12:23:57 +0200

    Create tornlogs_stop.js

---
+++ src/commands/torn/modules/tornlogs_stop.js
@@ -0,0 +1,24 @@
+// src/commands/torn/modules/tornlogs_stop.js
+import { MessageFlags, PermissionFlagsBits } from 'discord.js';
+import { successEmbed } from '../../../utils/embeds.js';
+import { replyUserError, ErrorTypes } from '../../../utils/errorHandler.js';
+import { setEnabled, stopTracking } from '../../../utils/tornLogTracker.js';
+
+export default {
+    async execute(interaction) {
+        if (!interaction.memberPermissions?.has(PermissionFlagsBits.ManageGuild)) {
+            return await replyUserError(interaction, {
+                type: ErrorTypes.PERMISSION,
+                message: 'Du benötigst die Berechtigung "Server verwalten".',
+            });
+        }
+
+        const stopped = stopTracking(interaction.guildId);
+        await setEnabled(interaction.guildId, false);
+
+        return await interaction.reply({
+            embeds: [successEmbed(stopped ? 'Torn-Log-Tracking gestoppt.' : 'Lief nicht.')],
+            flags: MessageFlags.Ephemeral,
+        });
+    }
+};
